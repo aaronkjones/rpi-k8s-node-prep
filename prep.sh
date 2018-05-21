68807df9849005c411616cfa2f43314d1bb78628
Aaron Jones <akjones@mailbox.org>
2018-05-21 13:28:30 -0700

    added defaults, fixed errors

--- prep.sh
+++ prep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-read -r -p "Is this a Master or Slave node (master/slave)? " node_choice
+read -r -p "Is this a Master or Slave node? (slave) " node_choice
 case "$node_choice" in
   master|Master ) node_type=master;;
   slave|Slave ) node_type=slave;;
-  * ) echo "Invalid entry."; exit 1;;
+  * ) node_type=slave;;
 esac
 
 echo "Installing Docker"
@@ -27,14 +27,15 @@ fi
 installed_docker_version=$(apt-cache policy docker-ce | grep "Installed" | cut -d ":" -f 2)
 echo 
 echo "$installed_docker_version is currently installed."
-read -r -p "Do you need to install a different version? (y/n) " docker_choice
+read -r -p "Do you need to install a different version? (no) " docker_choice
 echo
 case "$docker_choice" in
   y|Y ) apt-cache madison docker-ce | cut -d "|" -f 2 && \
   echo 
   read -r -p "Which Docker version do you want to install (e.g. 18.05.0~ce~3-0~ubuntu)? " docker_version && \
   sudo apt-get install -qy docker-ce="$docker_version";;
-  n|N )
+  n|N );;
+  * );;
 esac
 
 echo
@@ -55,29 +56,31 @@ echo
 
 installed_kuber_version=$(apt-cache policy kubeadm | grep "Installed" | cut -d ":" -f 2)
 echo "$installed_kuber_version is currently installed."
-read -r -p " Use latest Kubeadm version? (y/n) " kuber_choice
+read -r -p " Use latest Kubeadm version? (yes) " kuber_choice
 case "$kuber_choice" in
   n|N ) read -r -p "Which version of Kubernetes do you want to install (e.g. 1.10.2-00)? " kuber_version
   sudo apt-get install -qy kubeadm="$kuber_version" kubectl="$kuber_version" kubelet="$kuber_version";;
-  y|Y ) 
+  y|Y ) sudo apt-get install -qy kubeadm kubectl kubelet;;
 esac
 
 echo 
 
 echo "Docker $(apt-cache policy docker-ce | grep "Installed" | cut -d ":" -f 2) is installed"
-read -r -p "Do you want to prevent docker-ce from being upgraded? " upgrade_docker
+read -r -p "Do you want to prevent docker-ce from being upgraded? (no) " upgrade_docker
 case $upgrade_docker in
   y|Y ) echo "docker-ce hold" | sudo dpkg --set-selections; echo "done";;
-  n|N )
+  n|N );;
+  * );;
 esac
 
 echo 
 
 echo "Kubeadm $(apt-cache policy kubeadm | grep "Installed" | cut -d ":" -f 2) is installed"
-read -r -p "Do you want to prevent kubeadm from being upgraded? " upgrade_kuber
+read -r -p "Do you want to prevent kubeadm from being upgraded? (no) " upgrade_kuber
 case "$upgrade_kuber" in
   y|Y ) echo "kubeadm hold" | sudo dpkg --set-selections; echo "done";;
-  n|N )
+  n|N );;
+  * );;
 esac
 
 echo 
